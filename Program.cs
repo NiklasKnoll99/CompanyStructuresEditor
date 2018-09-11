fdb563e1c02e2899ed6e577e1fb3e4595816ee5f
Knolle2204 <niklas.knoll@tobit.software>
2018-09-11 17:47:47 +0200

    DbReader now returns table of data

--- Program.cs
+++ Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+
 namespace CompanyStructuresEditor
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DbReader dbReader = new DbReader("Server=TAPPQA;Database=Training-NK-Company;Trusted_Connection=true");
+            dbReader.Open();
+
+            DataTable s = dbReader.Read("SELECT * FROM viCompany"); // TEST
+            Console.Write(s.Rows[0][0]);
+
+            dbReader.Close();
+
+            Console.ReadKey(); // stop execution
         }
     }
 }
