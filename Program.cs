b042fa15fa24a1e96e6bd1ee857b6ec1ba96c8a0
Knolle2204 <niklas.knoll@tobit.software>
2018-09-12 09:08:03 +0200

    Create model "Company"

--- Program.cs
+++ Program.cs
@@ -12,14 +12,6 @@ namespace CompanyStructuresEditor
     {
         static void Main(string[] args)
         {
-            DbReader dbReader = new DbReader("Server=TAPPQA;Database=Training-NK-Company;Trusted_Connection=true");
-            dbReader.Open();
-
-            DataTable s = dbReader.Read("SELECT * FROM viCompany"); // TEST
-            Console.Write(s.Rows[0][0]);
-
-            dbReader.Close();
-
             Console.ReadKey(); // stop execution
         }
     }
