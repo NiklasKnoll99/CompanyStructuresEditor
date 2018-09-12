2d018b3f6b766d257f992dd725ae6de08d9e63db
Knolle2204 <niklas.knoll@tobit.software>
2018-09-12 09:13:11 +0200

    Create model "Employee"

---
+++ Model/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyStructuresEditor.Model
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Gender { get; set; }
+
+        public double Pay { get; set; }
+    }
+}
